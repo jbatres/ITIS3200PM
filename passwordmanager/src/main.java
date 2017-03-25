772d292e637d21028aeeaa3db5c76aec861e0965
jbatres <jbatres1@uncc.edu>
2017-03-25 15:12:08 -0400

    testing main

---
+++ passwordmanager/src/main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Jairo on 3/25/2017.
+ */
+public class main {
+        public static void main(String[] args) {
+            // Prints "Hello, World" to the terminal window.
+            System.out.println("Hello, World");
+        }
+
+
+}
